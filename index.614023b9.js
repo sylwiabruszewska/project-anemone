886c274f49f3bc601fb73d8198f77a25a9ef5b58
sylwiabruszewska <53376938+sylwiabruszewska@users.noreply.github.com>
2023-04-03 13:01:47 +0000

    Deploying to gh-pages from @ sylwiabruszewska/project-anemone@576b0cafa48efbf769a9c7c795a8366f8d2185a6 🚀

--- index.614023b9.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[newsletter-modal-open]"),closeModalBtn:document.querySelector("[newsletter-modal-close]"),modal:document.querySelector("[newsletter-modal]")};function t(){e.modal.classList.toggle("newsletter-is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)})();
-//# sourceMappingURL=index.614023b9.js.map
