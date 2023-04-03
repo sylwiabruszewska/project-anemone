07394d9e2ff31dc30ece17b5a6b672537d24ee6d
Andrzej Kubiczek <80128407+andrzejkubiczek@users.noreply.github.com>
2023-04-03 23:34:48 +0200

    upgrade #2
    changes in const names, add errorState and correctState

--- src/js/modal-newsletter.js
+++ src/js/modal-newsletter.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
+  const refs_newsletter = {
     openModalNewsletterBtn: document.querySelector('[data-modal-newsletter-open]'),
     closeModalNewsletterBtn: document.querySelector('[data-modal-newsletter-close]'),
     modalNewsletter: document.querySelector('[modal-newsletter]'),
   };
 
-  refs.openModalNewsletterBtn.addEventListener('click', toggleModalNewsletter);
-  refs.closeModalNewsletterBtn.addEventListener('click', toggleModalNewsletter);
+  refs_newsletter.openModalNewsletterBtn.addEventListener('click', toggleModalNewsletter);
+  refs_newsletter.closeModalNewsletterBtn.addEventListener('click', toggleModalNewsletter);
 
   function toggleModalNewsletter() {
-    refs.modalNewsletter.classList.toggle('is-hidden');
+    refs_newsletter.modalNewsletter.classList.toggle('is-hidden');
   }
 })();
--- src/js/modal-thx.js
+++ src/js/modal-thx.js
@@ -1,16 +1,16 @@
 (() => {
-  const refs = {
+  const refs_thx = {
     openModalThxBtn: document.querySelector('[data-modal-thx-open]'),
     closeModalThxBtn: document.querySelector('[data-modal-thx-close]'),
     closeContinueModalThxBtn: document.querySelector('[data-modal-thx-continue-close]'),
     modalThx: document.querySelector('[modal-thx]'),
   };
 
-  refs.openModalThxBtn.addEventListener('click', toggleModalThx);
-  refs.closeModalThxBtn.addEventListener('click', toggleModalThx);
-  refs.closeContinueModalThxBtn.addEventListener('click', toggleModalThx);
+  refs_thx.openModalThxBtn.addEventListener('click', toggleModalThx);
+  refs_thx.closeModalThxBtn.addEventListener('click', toggleModalThx);
+  refs_thx.closeContinueModalThxBtn.addEventListener('click', toggleModalThx);
 
   function toggleModalThx() {
-    refs.modalThx.classList.toggle('is-hidden');
+    refs_thx.modalThx.classList.toggle('is-hidden');
   }
 })();
