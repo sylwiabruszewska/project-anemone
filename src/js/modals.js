ff87b691fc18512e83008e1f5bdd6f9b38157eb5
Andrzej Kubiczek <80128407+andrzejkubiczek@users.noreply.github.com>
2023-04-04 22:53:48 +0200

    remove modal-hero, combine hero/store modal

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,13 +1,9 @@
 (() => {
   const refs = {
     // REFS
-    // hero contact refs
-    openModalBtnContactHero: document.querySelector('[modal-contact-hero-open]'),
-    closeModalBtnContactHero: document.querySelector('[modal-contact-hero-close]'),
-    modalContactHero: document.querySelector('[modal-contact-hero]'),
-
     // contact refs
     openModalBtnContact: document.querySelector('[modal-contact-open]'),
+    open2ModalBtnContact: document.querySelector('[modal-contact-open2]'),
     closeModalBtnContact: document.querySelector('[modal-contact-close]'),
     modalContact: document.querySelector('[modal-contact]'),
 
@@ -18,19 +14,15 @@
 
     // thank you refs
     openModalBtnThanks: document.querySelector('[modal-thanks-open]'),
-    open2ModalBtnThanks: document.querySelector('[modal-thanks-open2]'),
     closeModalBtnThanks: document.querySelector('[modal-thanks-close]'),
     continueModalBtnThanks: document.querySelector('[modal-thanks-continue]'),
     modalThanks: document.querySelector('[modal-thanks]'),
   };
 
   // ADD EVENT LISTENER
-  // hero contact event listener
-  refs.openModalBtnContactHero.addEventListener('click', toggleModalContactHero);
-  refs.closeModalBtnContactHero.addEventListener('click', toggleModalContactHero);
-
   // contact event listener
   refs.openModalBtnContact.addEventListener('click', toggleModalContact);
+  refs.open2ModalBtnContact.addEventListener('click', toggleModalContact);
   refs.closeModalBtnContact.addEventListener('click', toggleModalContact);
 
   // newsletter event listener
@@ -39,26 +31,15 @@
 
   // thank you event listener
   refs.openModalBtnThanks.addEventListener('click', () => {
-    refs.modalContactHero.classList.add('is-hidden');
-    toggleThankYouModal();
-  });
-
-  refs.open2ModalBtnThanks.addEventListener('click', () => {
     refs.modalContact.classList.add('is-hidden');
     toggleThankYouModal();
   });
 
   refs.openModalBtnThanks.addEventListener('click', toggleModalThanks);
-  refs.open2ModalBtnThanks.addEventListener('click', toggleModalThanks);
   refs.closeModalBtnThanks.addEventListener('click', toggleModalThanks);
   refs.continueModalBtnThanks.addEventListener('click', toggleModalThanks);
 
   // FUNCTION
-  // hero contact function
-  function toggleModalContactHero() {
-    refs.modalContactHero.classList.toggle('is-hidden');
-  }
-
   // contact function
   function toggleModalContact() {
     refs.modalContact.classList.toggle('is-hidden');
@@ -69,10 +50,6 @@
     refs.modalNewsletter.classList.toggle('is-hidden');
   }
 
-  // thank you function
-  // function toggleModalThx() {
-  //   refs.modalThx.classList.toggle('is-hidden');
-  // }
   function toggleModalThanks() {
     refs.modalThanks.classList.toggle('is-hidden');
   }
