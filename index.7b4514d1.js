bc22c6f6efd74a59f5f5eefef2967d7f01ef8427
sylwiabruszewska <53376938+sylwiabruszewska@users.noreply.github.com>
2023-04-06 00:55:55 +0000

    Deploying to gh-pages from @ sylwiabruszewska/project-anemone@33e0248a4064b95bc147f2bb3f11449e3e64a2ff 🚀

---
+++ index.7b4514d1.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",speed:500,centerInsufficientSlides:!0,slidesPerView:2,breakpoints:{320:{slidesPerView:2,spaceBetween:16},480:{slidesPerView:2,spaceBetween:16},768:{slidesPerView:2,spaceBetween:31},1280:{slidesPerView:3,spaceBetween:32}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.7b4514d1.js.map
